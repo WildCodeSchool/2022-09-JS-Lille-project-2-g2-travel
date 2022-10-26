232dd34f678c7a25bb1ce83b55506ac8d42e0d5b
alexis-Pouril <alexis.pouril@me.com>
2022-10-26 10:51:40 +0200

    initial cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import Home from "./pages/Home";
-
+import "./reset.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <p>coucou</p>
     </div>
   );
