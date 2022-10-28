16c5df0e542ff44291909e58031d6b47991fa946
alexis-Pouril <alexis.pouril@me.com>
2022-10-28 14:48:57 +0200

    Add card components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import "./reset.css";
 import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
-      <p>Coucou wilder</p>
+      <Card
+        Title="Card Title"
+        ImageUrl="cdn.pixabay.com/photo/2022/09/30/10/05/river-7489170_1280.jpg"
+        Body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nisi recusandae, sed quidem quod, in, iure assumenda quis maxime voluptatibus nemo quo inventore non ea deleniti? Nobis aperiam quae ea."
+      />
     </div>
   );
 }
