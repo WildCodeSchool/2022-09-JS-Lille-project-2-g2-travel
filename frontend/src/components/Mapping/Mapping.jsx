8c1d3547bb9497bc52bfc3c866f8059792f067bc
cartige <cartiaux.gerald@gmail.com>
2022-11-03 14:27:25 +0100

    integrated custom marker in map

--- frontend/src/components/Mapping/Mapping.jsx
+++ frontend/src/components/Mapping/Mapping.jsx
@@ -2,8 +2,21 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { PropTypes } from "prop-types";
 import "./Mapping.css";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "../../assets/iconTravel.png";
 
 export default function Mapping({ pointsOfInterest, center }) {
+  const iconTravel = new L.Icon({
+    iconUrl: icon,
+    iconRetinaUrl: icon,
+    iconAnchor: [12, 41],
+    popupAnchor: [-4, -47],
+    shadowUrl: icon,
+    shadowSize: new L.Point(18, 37),
+    shadowAnchor: [12, 41],
+    iconSize: new L.Point(18, 37),
+  });
+
   return (
     <MapContainer
       center={center}
@@ -17,7 +30,11 @@ export default function Mapping({ pointsOfInterest, center }) {
       />
       {pointsOfInterest.map((pointOfInterest) => {
         return (
-          <Marker key={pointOfInterest.id} position={pointOfInterest.position}>
+          <Marker
+            key={pointOfInterest.id}
+            position={pointOfInterest.position}
+            icon={iconTravel}
+          >
             <Popup>
               <h1>{pointOfInterest.name}</h1>
               <sub>{pointOfInterest.category}</sub>
