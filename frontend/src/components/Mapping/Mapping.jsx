c1d75244c607849d0b158cce243492322fb952a2
cartige <cartiaux.gerald@gmail.com>
2022-11-10 10:51:10 +0100

    gap between map and cards on pages map, components Mapping style fix

--- frontend/src/components/Mapping/Mapping.jsx
+++ frontend/src/components/Mapping/Mapping.jsx
@@ -1,8 +1,6 @@
-// eslint-disable-next-line import/no-unresolved
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { PropTypes } from "prop-types";
 import "./Mapping.css";
-// eslint-disable-next-line import/no-unresolved
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "../../assets/iconTravel.png";
@@ -14,9 +12,9 @@ export default function Mapping({ pointsOfInterest, center }) {
     iconAnchor: [12, 41],
     popupAnchor: [-4, -47],
     shadowUrl: icon,
-    shadowSize: new L.Point(18, 37),
+    shadowSize: new L.Point(23, 37),
     shadowAnchor: [12, 41],
-    iconSize: new L.Point(18, 37),
+    iconSize: new L.Point(23, 37),
   });
 
   return (
@@ -24,12 +22,21 @@ export default function Mapping({ pointsOfInterest, center }) {
       center={center}
       zoom={13}
       scrollWheelZoom
-      style={{ height: "500px", width: "500px", borderRadius: "3%" }}
+      style={{
+        height: "32rem",
+        borderRadius: "1rem",
+        minWidth: "60%",
+      }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={center} icon={iconTravel}>
+        <Popup>
+          <h1>Your Position</h1>
+        </Popup>
+      </Marker>
       {pointsOfInterest.map((pointOfInterest) => {
         return (
           <Marker
