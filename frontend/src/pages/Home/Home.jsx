d9a4e48bc1ec20ada0e0e88047e087cca23bb9a7
cartige <cartiaux.gerald@gmail.com>
2022-11-10 12:23:26 +0100

    reorganizing components in page Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,22 +1,35 @@
+import { useState } from "react";
 import Header from "../../components/Header/Header";
-import SearchComponent from "../../components/SearchComponent/SearchComponent";
 import voyage from "../../assets/voyages-2.jpg";
 import Carousel from "../../components/Carousel";
 import "./Home.css";
 
 export default function Home() {
+  const [resultSearch, setResultSearch] = useState("");
+  const handleInput = (e) => {
+    setResultSearch(e.target.value);
+  };
+
   return (
     <div className="home">
       <Header />
       <div className="main-home">
         <div className="search">
-          <SearchComponent />
+          <input
+            type="search"
+            id="search"
+            name="search-home"
+            placeholder="Quelle destination?"
+            value={resultSearch}
+            onChange={handleInput}
+          />
         </div>
-        <img src={voyage} alt="" />
+        <img src={voyage} alt="main-img-home" />
       </div>
-      <div className="carousel">
+      <div className="carousel-home">
         <Carousel />
       </div>
+      <div className="footer-home"> </div>
     </div>
   );
 }
