9d2ac8dc181dbf7f1dd40716c67b1eabe3719c61
Romain HADDAD <108538558+Azahires@users.noreply.github.com>
2022-11-22 15:15:19 +0100

    Update Location.jsx

--- frontend/src/components/Location/Location.jsx
+++ frontend/src/components/Location/Location.jsx
@@ -12,19 +12,3 @@ export default function Location() {
   );
 }
 
-// // export default function Location() {
-// //   const [location, setLocation] = useState("");
-// //   useEffect(() => {
-// //     axios
-// //       .get(
-// //         `https://api.opentripmap.com/0.1/en/places/geoname?name=${location}&apikey=5ae2e3f221c38a28845f05b68cde73439c2e803dfd3bb7501b78d084`
-// //       )
-// //       .then(({ data }) => {
-// //         setLocation(data.name);
-// //       });
-// //   }, []);
-// //   return <div>{location} </div>;
-// // }
-// Location.propTypes = {
-//   cities: PropTypes.string.isRequired,
-// };
