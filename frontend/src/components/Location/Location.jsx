a26ae62d3a1194de5e20bab6896958ebe1c6de18
Priscillia <selvapriscillia@yahoo.fr>
2022-11-22 15:44:26 +0100

    correction comments

--- frontend/src/components/Location/Location.jsx
+++ frontend/src/components/Location/Location.jsx
@@ -1,7 +1,4 @@
 import "./location.css";
-// import { PropTypes } from "prop-types";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
 
 export default function Location() {
   return (
@@ -11,20 +8,3 @@ export default function Location() {
     </div>
   );
 }
-
-// // export default function Location() {
-// //   const [location, setLocation] = useState("");
-// //   useEffect(() => {
-// //     axios
-// //       .get(
-// //         `https://api.opentripmap.com/0.1/en/places/geoname?name=${location}&apikey=5ae2e3f221c38a28845f05b68cde73439c2e803dfd3bb7501b78d084`
-// //       )
-// //       .then(({ data }) => {
-// //         setLocation(data.name);
-// //       });
-// //   }, []);
-// //   return <div>{location} </div>;
-// // }
-// Location.propTypes = {
-//   cities: PropTypes.string.isRequired,
-// };
